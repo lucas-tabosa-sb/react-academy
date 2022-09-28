222f90ff6f50d2b1c4227474d55e2ef1cc158287
Lucas Tabosa <lucas.tabosa.sb@gmail.com>
2022-09-28 19:49:18 -0300

    implements search
    todo:
    
    refactor app, but main function is working

--- delivery1/src/App.js
+++ delivery1/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { useState } from "react";
+import data from './components/data'
 
 function App() {
+  let discoveredPokemon = []
 
-
-  const [pokemon, setPokemon] = useState("Blastoise");
+  const pokenames = data.map((poke) =>  discoveredPokemon.push(poke.name))
+  
+  const [pokemon, setPokemon] = useState("");
 
   const searchPokemon = () => {
     const searchField = document.getElementById("poke-search");
@@ -13,10 +16,12 @@ function App() {
 
   return (
     <div className="App">
-      <input type="search" id="poke-search" />
+      <input type="search" id="poke-search" placeholder="search the name of the pokemon, e.g: Pikachu"/>
       <button onClick={searchPokemon}>Search</button>
       <div>
-        <img src={require(`../public/assets/${pokemon}.png`)} alt="pokemon"/>
+        { discoveredPokemon.includes(`${pokemon}`) ?
+          <img src={require(`../public/assets/${pokemon}.png`)} alt="pokemon"/> : <h2>Sorry, this pokemon hasn't been discovered</h2>
+        }
       </div>
     </div>
   );
